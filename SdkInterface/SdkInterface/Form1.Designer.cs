c7cb3e66b6b91890d9663144d4ebfe79fdde3bea
Santiago Marchisio <santimarchisio_94@hotmail.com>
2018-11-26 10:06:05 -0300

    Add sdk interface for testing

--- SdkInterface/SdkInterface/Form1.Designer.cs
+++ SdkInterface/SdkInterface/Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.capacityLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.tokenTextBox = new System.Windows.Forms.TextBox();
+            this.evaluateButton = new System.Windows.Forms.Button();
+            this.userIdTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // capacityLabel
+            // 
+            this.capacityLabel.AutoSize = true;
+            this.capacityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.capacityLabel.Location = new System.Drawing.Point(52, 149);
+            this.capacityLabel.Margin = new System.Windows.Forms.Padding(6);
+            this.capacityLabel.Name = "capacityLabel";
+            this.capacityLabel.Size = new System.Drawing.Size(90, 29);
+            this.capacityLabel.TabIndex = 8;
+            this.capacityLabel.Text = "User id";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(52, 102);
+            this.nameLabel.Margin = new System.Windows.Forms.Padding(6);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(126, 29);
+            this.nameLabel.TabIndex = 5;
+            this.nameLabel.Text = "Flag token";
+            // 
+            // tokenTextBox
+            // 
+            this.tokenTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tokenTextBox.Location = new System.Drawing.Point(324, 102);
+            this.tokenTextBox.Margin = new System.Windows.Forms.Padding(6);
+            this.tokenTextBox.Name = "tokenTextBox";
+            this.tokenTextBox.Size = new System.Drawing.Size(414, 35);
+            this.tokenTextBox.TabIndex = 6;
+            // 
+            // evaluateButton
+            // 
+            this.evaluateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.evaluateButton.Location = new System.Drawing.Point(324, 223);
+            this.evaluateButton.Margin = new System.Windows.Forms.Padding(6);
+            this.evaluateButton.Name = "evaluateButton";
+            this.evaluateButton.Size = new System.Drawing.Size(162, 56);
+            this.evaluateButton.TabIndex = 7;
+            this.evaluateButton.Text = "Evaluate";
+            this.evaluateButton.UseVisualStyleBackColor = true;
+            this.evaluateButton.Click += new System.EventHandler(this.evaluateButton_Click);
+            // 
+            // userIdTextBox
+            // 
+            this.userIdTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userIdTextBox.Location = new System.Drawing.Point(324, 149);
+            this.userIdTextBox.Margin = new System.Windows.Forms.Padding(6);
+            this.userIdTextBox.Name = "userIdTextBox";
+            this.userIdTextBox.Size = new System.Drawing.Size(414, 35);
+            this.userIdTextBox.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(806, 391);
+            this.Controls.Add(this.capacityLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.tokenTextBox);
+            this.Controls.Add(this.evaluateButton);
+            this.Controls.Add(this.userIdTextBox);
+            this.Name = "Form1";
+            this.Text = "Evaluate Flag";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label capacityLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox tokenTextBox;
+        private System.Windows.Forms.Button evaluateButton;
+        private System.Windows.Forms.TextBox userIdTextBox;
     }
 }
 
--- SdkInterface/SdkInterface/Form1.cs
+++ SdkInterface/SdkInterface/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EvaluateSdk;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,25 @@ namespace SdkInterface
 {
     public partial class Form1 : Form
     {
+
+        EvaluateSdk.EvaluateFlag sdk = new EvaluateSdk.EvaluateFlag();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void evaluateButton_Click(object sender, EventArgs e)
+        {
+            bool result = sdk.Evaluate(tokenTextBox.Text, userIdTextBox.Text);
+            MessageBox.Show("Result: " + result,
+                            "Evaluate",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
         }
     }
 }
